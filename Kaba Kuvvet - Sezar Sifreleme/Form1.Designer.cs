8498b67873d148922e32967f760c4164915e39a5
Serdar Eyüp ALTIN <43209941+serdaraltin@users.noreply.github.com>
2018-12-03 03:40:45 +0300

    Add files via upload

---
+++ Kaba Kuvvet - Sezar Sifreleme/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace Kaba_Kuvvet___Sezar_Sifreleme
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tx_sifrelimesaj = new System.Windows.Forms.TextBox();
+            this.rctx_acikmesaj = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(152, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Şifrelenmiş Mesaj (cipher text) :";
+            // 
+            // tx_sifrelimesaj
+            // 
+            this.tx_sifrelimesaj.Location = new System.Drawing.Point(170, 13);
+            this.tx_sifrelimesaj.Name = "tx_sifrelimesaj";
+            this.tx_sifrelimesaj.Size = new System.Drawing.Size(233, 20);
+            this.tx_sifrelimesaj.TabIndex = 1;
+            // 
+            // rctx_acikmesaj
+            // 
+            this.rctx_acikmesaj.Location = new System.Drawing.Point(170, 69);
+            this.rctx_acikmesaj.Name = "rctx_acikmesaj";
+            this.rctx_acikmesaj.Size = new System.Drawing.Size(233, 236);
+            this.rctx_acikmesaj.TabIndex = 2;
+            this.rctx_acikmesaj.Text = "";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(73, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Olası Açık Mesaj :";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(170, 39);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Saldır";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(417, 317);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.rctx_acikmesaj);
+            this.Controls.Add(this.tx_sifrelimesaj);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tx_sifrelimesaj;
+        private System.Windows.Forms.RichTextBox rctx_acikmesaj;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Kaba Kuvvet - Sezar Sifreleme/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kaba_Kuvvet___Sezar_Sifreleme
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for(int anahtar = 0; anahtar < 26; anahtar++)
+            {
+
+                rctx_acikmesaj.Text += anahtar.ToString() + " >> ";
+                foreach(char karekter in tx_sifrelimesaj.Text)
+                {
+                    int alfabe_sirasi = (int)karekter - 97;
+
+                    while(alfabe_sirasi - anahtar < 0) // eksi bir deger varsa
+                        alfabe_sirasi += 26;  // a = 0 , 0 - 1 = -1 , -1 + 26 = 25 = z
+
+                    rctx_acikmesaj.Text += (char)(alfabe_sirasi - anahtar + 97);
+                }
+                rctx_acikmesaj.Text += Environment.NewLine;
+
+            }
+        }
+    }
+}
